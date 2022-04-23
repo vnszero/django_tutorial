6954ba98127968894de3f18367873464d7334063
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-04-23 19:31:15 -0300

    SQLite3 DataBase Tutorial

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Tech with Tim!</h1>")
-
-def v1(response):
-    return HttpResponse("<h1>View 1!</h1>")    
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1></br><p>%s</p>" %(ls.name, str(item.text)))
