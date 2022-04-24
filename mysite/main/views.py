0f2736ac71c87e443b7b7ea8342440924b37fa42
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-04-23 21:24:00 -0300

    Admin Dashboard

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,5 +5,6 @@ from .models import ToDoList, Item
 
 def index(response, name):
     ls = ToDoList.objects.get(name=name)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1></br><p>%s</p>" %(ls.name, str(item.text)))
+    # item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1>" %ls.name)
+    # return HttpResponse("<h1>%s</h1></br><p>%s</p>" %(ls.name, str(item.text)))
