ffcc60ac654c8bf763ecbdd8bba9353a73a5f444
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-04-24 17:35:14 -0300

    Simple Forms

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("create/", views.create, name="create")
 ]
