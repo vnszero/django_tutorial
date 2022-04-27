8e7594bd6d4be93d951c2e1afbea30f03c0e9624
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2022-04-26 22:46:52 -0300

    User Specific Pages/Access

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,25 +6,26 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+    if ls in response.user.todolist.all():
+        if response.method == "POST":
+            print(response.POST)
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
 
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete = False)
                 else:
-                    item.complete = False
-                item.save()
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
+                    print("invalid")
 
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete = False)
-            else:
-                print("invalid")
-
-    return render(response, "main/list.html", {"ls":ls})
+        return render(response, "main/list.html", {"ls":ls})
+    return render(response, 'main/view.html', {})
 
 def home(response):
     return render(response, "main/home.html", {})
@@ -36,8 +37,12 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
         
         return HttpResponseRedirect("/%i" %t.id)
     else:
         form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
+
+def view(response):
+    return render(response, 'main/view.html', {})
